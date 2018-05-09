abb81bc3aabfa25163371f47ee69040a5fe3ae44
Poline <krasnova.poline@gmail.com>
2018-05-09 15:33:29 +0300

    add buttons and update current index feature

--- src/client/modules/Main/index.js
+++ src/client/modules/Main/index.js
@@ -2,29 +2,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './Main.scss';
 import defaultData from './defaultData';
-import { setData } from '../../reducers/main';
+import { setData, updateCurrentIndex } from '../../reducers/main';
 
 class Main extends React.Component{
   componentDidMount(){
     this.props.setData(defaultData);
   }
 
+  changeCurrentEl(current){
+    this.props.updateCurrentIndex(current);
+  }
+
   render () {
+    const { data } = this.props;
+    debugger
     return(
       <div>
-        Hello world!
+        {data.map((el, index) => (
+          <button key={`button${index}`} onClick={()=>{this.changeCurrentEl(index)}}>
+            {index + 1} {index + 1 > 1 ? 'elements' : 'element'}
+          </button>
+        ))}
       </div>
     )
   }
 };
 
 const mapStateToProps = (state) => ({
-
+  data: state.main.data,
+  current: state.main.current,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setData: (credentials) => {
-    dispatch(setData(credentials));
+  setData: (data) => {
+    dispatch(setData(data));
+  },
+  updateCurrentIndex: (currentIndex) => {
+    dispatch(updateCurrentIndex(currentIndex));
   },
 });
 
--- src/client/reducers/main.js
+++ src/client/reducers/main.js
@@ -1,25 +1,30 @@
 import dataHelper from '../lib/dataHelper';
 //TYPES
 const SET_DATA = 'SET_DATA';
+const UPDATE_CURRENT_INDEX = 'UPDATE_CURRENT_INDEX';
 
 //REDUCER
 export default function reducer(state = defaultState, action = {}) {
   switch (action.type) {
     case SET_DATA:
-        const validateData = dataHelper.getValidateData(action.credentials);
-        
+        const validateData = dataHelper.getValidateData(action.data);
         return Object.assign({}, state, {data: validateData});
+    case UPDATE_CURRENT_INDEX:
+        return Object.assign({}, state, action);
     default: return state;
   }
 }
 
 //ACTIONS
-export const setData = (credentials) => {
-    return { type: SET_DATA, credentials };
+export const setData = (data) => {
+    return { type: SET_DATA, data };
+}
+export const updateCurrentIndex = (currentIndex) => {
+    return { type: UPDATE_CURRENT_INDEX, currentIndex };
 }
 
 
 const defaultState = {
     data: [],
-    current: 0,
+    currentIndex: 0,
 };
